9bfc5aeb7c348a32c94bb37f1005699828c5d415
Ulxses15 <hernandezulises127@gmail.com>
2024-09-01 20:22:42 -0700

    test commit

--- src/CPU.java
+++ src/CPU.java
@@ -16,6 +16,7 @@ public class CPU {
 		
 		System.out.println("CPU Brand: " + brand.getName());
 		System.out.println("CPU Model: " + brand.getModel());
+		System.out.println("Hello world");
         
 	}
 }
